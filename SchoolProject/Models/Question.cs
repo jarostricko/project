18a91304ee38cdfe35e28a9faf2c95a7681ddc91
jarostricko <jarko.stricko@gmail.com>
2015-06-16 10:05:41 +0200

    question valid

--- SchoolProject/Models/Question.cs
+++ SchoolProject/Models/Question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace SchoolProject.Models
 {
     public class Question
     {
+        /*
         public int QuestionID { get; set; }
         public string Text { get; set; }
         public virtual List<Answer> AnswersList { get; set; }
@@ -19,20 +21,19 @@ namespace SchoolProject.Models
 
         public virtual List<TestTemplate> TestTemplates { get; set; }
         public virtual ThematicField ThematicField { get; set; }
+        */
 
-        /*public int QuestionID { get; set; }
+        public int QuestionID { get; set; }
         [Required]
         [Display(Name = "Question Text")]
         [StringLength(250)]
         public string Text { get; set; }
         public virtual List<Answer> AnswersList { get; set; }
         public int Points { get; set; }
-        //[Required]
         [StringLength(150)]
         public string Explanation { get; set; }
-        [Required]
         public int ThematicFieldID { get; set; }
         public virtual List<TestTemplate> TestTemplates { get; set; }
-        public virtual ThematicField ThematicField { get; set; }*/
+        public virtual ThematicField ThematicField { get; set; }
     }
 }
