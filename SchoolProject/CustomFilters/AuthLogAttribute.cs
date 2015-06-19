0ab1546639784c5f0e45526a2e8c91949b6033a9
jarostricko <jarko.stricko@gmail.com>
2015-06-19 11:11:27 +0200

    Auth complete working

--- SchoolProject/CustomFilters/AuthLogAttribute.cs
+++ SchoolProject/CustomFilters/AuthLogAttribute.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using SchoolProject.DAL;
+using SchoolProject.Models;
 
 namespace SchoolProject.CustomFilters
 {
@@ -22,14 +24,7 @@ namespace SchoolProject.CustomFilters
         /// <param name="filterContext"></param>
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
-            //base.OnAuthorization(filterContext);
-            var users = base.Users;
-            var userID = filterContext.HttpContext.User.Identity.GetUserId();
-            var userRoles = filterContext.HttpContext.User.IsInRole(Roles);
-            if (filterContext.HttpContext.User.IsInRole(Roles))
-            {
-                return;
-            }
+            base.OnAuthorization(filterContext);
             IsUserAuthorized(filterContext);
         }
 
@@ -40,6 +35,17 @@ namespace SchoolProject.CustomFilters
         /// <param name="filterContext"></param>
         private void IsUserAuthorized(AuthorizationContext filterContext)
         {
+            
+            var currentUserId = filterContext.HttpContext.User.Identity.GetUserId();
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new SchoolProjectContext()));
+            var currentUser = manager.FindById(currentUserId);
+            //toto riesenie z dovodu, ze tieto Authozation metody hladaju Role a Usera v inej tabulke
+            //Userov a Role a vztahy medzi nimi su v tabulkach IdentityRole a pod. a tieto metody to hladaju v AspNetRoles apod.
+            //Niekolko hodin som hladal riesenie a nenasiel som ho, tak som to spravil takto
+            if (currentUser != null && currentUser.IsTeacher && Roles == "Teacher") 
+            {
+                filterContext.Result = null;
+            }
             // If the Result returns null then the user is Authorized 
             if (filterContext.Result == null)
                 return;
