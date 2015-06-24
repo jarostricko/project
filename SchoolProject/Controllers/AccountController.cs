f1821eba1576374295f12df231f6812018447371
jarostricko <jarko.stricko@gmail.com>
2015-06-24 22:14:40 +0200

    details

--- SchoolProject/Controllers/AccountController.cs
+++ SchoolProject/Controllers/AccountController.cs
@@ -448,7 +448,7 @@ namespace SchoolProject.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Teacher");
                 }
                 AddErrors(result);
             }
@@ -497,7 +497,7 @@ namespace SchoolProject.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Student");
                 }
                 AddErrors(result);
             }
